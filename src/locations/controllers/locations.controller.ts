62fd874b423db8f2937ed8a731b4508bd3c032e7
Marcos Espinoza <mespinoza@waverleysoftware.com>
2023-08-18 16:25:03 -0400

    create all endpoints for locations

--- src/locations/controllers/locations.controller.ts
+++ src/locations/controllers/locations.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('api/locations')
 export class LocationsController {
   @Get()
   getAll() {
     return [1, 2, 3];
+  }
+
+  @Get(':id')
+  getSingle(@Param('id') id: number) {
+    return id;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
   }
 }
